e1b8403f9c1c7768832a8d8e42b88b4995cd9ae3
FPC <354412@via.dk>
2026-05-11 12:21:42 +0200

    Update DLL to have ML-Models (#1)
    Also added some NOT NULL's in there...

--- DDL.sql
+++ DDL.sql
@@ -2,7 +2,7 @@ CREATE SCHEMA IF NOT EXISTS sep4dk1;
 SET SCHEMA 'sep4dk1';
 
 CREATE TABLE IF NOT EXISTS "Weather"(
-    time BIGINT,
+    time BIGINT NOT NULL,
     temperature FLOAT,
     humidity FLOAT,
     wind_direction INT,
@@ -12,14 +12,27 @@ CREATE TABLE IF NOT EXISTS "Weather"(
     PRIMARY KEY (time)
 );
 
+CREATE TABLE IF NOT EXISTS "Model"(
+    name VARCHAR(3) NOT NULL,
+    major_version INT NOT NULL,
+    minor_version INT NOT NULL,
+    trained_at BIGINT,
+    PRIMARY KEY (name, major_version, minor_version)
+);
+
 CREATE TABLE IF NOT EXISTS "WeatherPrediction"(
-    predicted_time BIGINT,
-    prediction_offset INT,
+    predicted_time BIGINT NOT NULL,
+    prediction_offset INT NOT NULL,
     temperature FLOAT,
     humidity FLOAT,
     wind_direction INT,
     wind_speed FLOAT,
     precipitation FLOAT,
     light INT,
-    PRIMARY KEY (predicted_time, prediction_offset)
+    model_name VARCHAR(3) NOT NULL,
+    model_major_version INT NOT NULL,
+    model_minor_version INT NOT NULL,
+    PRIMARY KEY (predicted_time, prediction_offset, model_name, model_major_version, model_minor_version),
+    FOREIGN KEY (model_name, model_major_version, model_minor_version) REFERENCES "Model"(name, major_version, minor_version)
 );
+
